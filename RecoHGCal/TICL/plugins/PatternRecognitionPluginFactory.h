8c9a0d5bb624f6974da822c77baf4c77876f506a
Mark <m.matthewman@gmail.com>
2023-05-07 21:50:22 +0200

    Delete changes

--- RecoHGCal/TICL/plugins/PatternRecognitionPluginFactory.h
+++ RecoHGCal/TICL/plugins/PatternRecognitionPluginFactory.h
@@ -14,6 +14,4 @@ typedef edmplugin::PluginFactory<ticl::PatternRecognitionAlgoBaseT<TICLLayerTile
                                                                                           edm::ConsumesCollector)>
     PatternRecognitionHFNoseFactory;
 
-
-
 #endif
