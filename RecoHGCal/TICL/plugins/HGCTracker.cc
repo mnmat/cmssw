460ed85ebc39d2b81f5345b581afda015d83972f
Mark <m.matthewman@gmail.com>
2023-07-26 13:26:15 +0200

    Fix nextDisk
    Previously nextDisk only allowed iteration over 46 layers due to a condition left from the prior navigation. Removing it solved the issue.

--- RecoHGCal/TICL/plugins/HGCTracker.cc
+++ RecoHGCal/TICL/plugins/HGCTracker.cc
@@ -18,56 +18,36 @@ HGCTracker::HGCTracker(const CaloGeometry* geom,
                             radlen_(radlen),
                             xi_(xi)  {
 
+  rhtools_.setGeometry(*geom);
+  int hgcalEEId = DetId::HGCalEE;
+  int hgcalHSiId = DetId::HGCalHSi;
+  int hgcalHScId = DetId::HGCalHSc;
 
-    rhtools_.setGeometry(*geom);
-    int hgcalEEId = DetId::HGCalEE;
-    int hgcalHSiId = DetId::HGCalHSi;
-    int hgcalHScId = DetId::HGCalHSc;
+  std::vector<HGCDiskGeomDet *> disksSiPos, disksScPos;
+  std::vector<HGCDiskGeomDet *> disksSiNeg, disksScNeg;
 
-    std::vector<HGCDiskGeomDet *> disksSiPos, disksScPos;
-		std::vector<HGCDiskGeomDet *> disksSiNeg, disksScNeg;
+  makeDisks(hgcalEEId, geom,disksSiPos, disksSiNeg);
+  makeDisks(hgcalHSiId, geom,disksSiPos, disksSiNeg);
+  makeDisks(hgcalHScId, geom,disksScPos, disksScNeg);
 
-    makeDisks(hgcalEEId, geom,disksSiPos, disksSiNeg);
-    makeDisks(hgcalHSiId, geom,disksSiPos, disksSiNeg);
-    makeDisks(hgcalHScId, geom,disksScPos, disksScNeg);
+  auto ptrSort = [](const HGCDiskGeomDet *a, const HGCDiskGeomDet *b) -> bool { return (abs(a->position().z())) < (abs(b->position().z())); };
+  std::sort(disksSiPos.begin(), disksSiPos.end(), ptrSort);
+  std::sort(disksSiNeg.begin(), disksSiNeg.end(), ptrSort);
+  std::sort(disksScPos.begin(), disksScPos.end(), ptrSort);
+  std::sort(disksScNeg.begin(), disksScNeg.end(), ptrSort);
 
-    auto ptrSort = [](const HGCDiskGeomDet *a, const HGCDiskGeomDet *b) -> bool { return (abs(a->position().z())) < (abs(b->position().z())); };
-    std::sort(disksSiPos.begin(), disksSiPos.end(), ptrSort);
-    std::sort(disksSiNeg.begin(), disksSiNeg.end(), ptrSort);
-    std::sort(disksScPos.begin(), disksScPos.end(), ptrSort);
-    std::sort(disksScNeg.begin(), disksScNeg.end(), ptrSort);
-
-    offset = disksSiPos.size()-disksScPos.size();
-    std::cout << "Exited makeDisks" << std::endl;
-    std::cout << disksSiPos.size() << std::endl;
-    std::cout << disksSiNeg.size() << std::endl;
-    std::cout << disksScPos.size() << std::endl;
-    std::cout << disksScNeg.size() << std::endl;
-
-    std::cout << "Sorted makeDisks" << std::endl;
-    makeDiskLayers(disksSiPos,disksScPos);
-		makeDiskLayers(disksSiNeg,disksScNeg);
-
-    for(auto disk: diskLayerPos_){
-      if (disk->first){
-        std::cout << disk->first->layer() << ", " << disk->second->layer() << std::endl;
-      } else{
-        std::cout << disk->second->layer() << std::endl;
-      }
-    }
-
+  offset = disksSiPos.size()-disksScPos.size();
+  makeDiskLayers(disksSiPos,disksScPos);
+  makeDiskLayers(disksSiNeg,disksScNeg);
 }
 
 HGCTracker::~HGCTracker(){}
 
 void HGCTracker::makeDiskLayers(std::vector<HGCDiskGeomDet*>disksSi, std::vector<HGCDiskGeomDet*> disksSc){
-  std::cout << "Offset: " << offset  << std::endl;
 	for(int layer=0; layer<lastLayer; layer++){
-    std::cout << layer << std::endl;
 		if (layer<offset){
 			addDiskLayer(disksSi[layer]);
 		} else{
-      std::cout << "Bla" << std::endl;
 			addDiskLayer(disksSi[layer],disksSc[layer-offset]);
 		}
 	}
@@ -99,9 +79,7 @@ void HGCTracker::makeDisks(int subdet, const CaloGeometry* geom_, std::vector<HG
 
   int layer = ddd->getLayerOffset(); // FIXME: Can be made less stupid
   if (subdet == DetId::HGCalHSc) offset = ddd->getLayerOffset();
-  std::cout << "------------------- Layer Offset: " << layer << ", NumDisks: " << numdisks << std::endl;
   for (int i = 0; i < numdisks; ++i) {
-    std::cout << layer << std::endl;
     if (countPos[i]) {
       HGCDiskGeomDet* disk = new HGCDiskGeomDet(subdet, +1, layer+1, zsumPos[i]/countPos[i], rmin[i], rmax[i], radlen_[layer], xi_[layer]);
       disksPos.push_back(disk);
@@ -118,21 +96,16 @@ const HGCDiskLayer* HGCTracker::nextDisk(const HGCDiskLayer * from,
                                           PropagationDirection direction, 
                                           bool isSilicon) const{
 
-  std::cout << "Entered nextDisk" << std::endl;
-  //std::cout << from->
   const std::vector<HGCDiskLayer *> & vec = (from->second->zside() > 0 ? diskLayerPos_ : diskLayerNeg_);
   auto it = std::find(vec.begin(), vec.end(),from);
   if (it == vec.end()) throw cms::Exception("LogicError", "nextDisk called with invalid starting disk"); 
   if (direction == alongMomentum){
-    if ((*it == vec.back()) || (*it == vec.rbegin()[1])) return nullptr; // if disk last silicon OR last scintillator disk
-    std::cout << "exit next Disk" << std::endl;
+    if (*it == vec.back()) return nullptr; // if disk last silicon OR last scintillator disk
     return *(++it);
   } else{
     if (it == vec.begin()) return nullptr;
-      std::cout << "exit next Disk" << std::endl;
       return *(--it);
   }
-  std::cout << "exit next Disk" << std::endl;
   return nullptr; // Returns nullptr if nextDisk reached end of detector
 }
 
